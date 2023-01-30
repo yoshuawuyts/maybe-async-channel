c35d43a541e5c5cef30f22b72c00e14856372676
Yoshua Wuyts <2467194+yoshuawuyts@users.noreply.github.com>
2023-01-30 14:42:23 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! A channel which may or may not be async
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
